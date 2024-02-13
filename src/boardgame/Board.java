431b4bb863bdccc7309b25f797dd6cc8679a34a9
Vinicius Oliveira <viniciusoliveira.270304@gmail.com>
2024-02-13 02:05:11 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -33,6 +33,18 @@ public class Board {
 		this.columns = columns;
 	}
 	
+	//metodos
+	
+	public Piece piece (int row, int column) {
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
+	
+	
+	
 	
 	
 	
